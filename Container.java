020a1662db22b25b11bbd9f020a16fa688c9ff7b
trungluong0806 <141601693+trungluong080699@users.noreply.github.com>
2023-08-28 11:18:52 +0700

    Update Container.java

--- Container.java
+++ Container.java
@@ -1,14 +1,13 @@
+import java.io.*;
 import java.util.Scanner;
 import java.util.Random;
 import java.util.ArrayList;
-public class Container {
+public class Container implements Serializable {
     private String Cid;
     private String name;
     private double weight;
     private double fuel_consumption_per_km_on_ship;
     private double fuel_consumption_per_km_on_truck;
-    private static ArrayList<Container> container_list = new ArrayList<Container>();
-
 
 
 
@@ -19,11 +18,39 @@ public class Container {
     }
 
     public String toString(){
-        return "Container id: " + this.Cid + "\n" + "Container name: " + this.name + "\n" + "Weight: " + this.weight + "\n" + "Per Km Fuel Consumption on Ship " + this.fuel_consumption_per_km_on_ship + "\n" + "Per Km Fuel Consumption on Truck: " + this.fuel_consumption_per_km_on_truck + "\n" + "Date of Departure: ";}
+        return "Container id: " + this.Cid + "\n" + "Container name: " + this.name + "\n" + "Weight: " + this.weight + "\n" + "Per Km Fuel Consumption on Ship " + this.fuel_consumption_per_km_on_ship + "\n" + "Per Km Fuel Consumption on Truck: " + this.fuel_consumption_per_km_on_truck + "\n";}
 
+    public static void inputContainerIntoFile(File file, Container container){
+        if (file.length() == 0 ){
+            try{
+                ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream("container.txt"));
+                oos.writeObject(container);
+                oos.close();
+
+                oos.close();
+            } catch (FileNotFoundException e){
+                e.printStackTrace();
+            } catch (IOException e){
+                e.printStackTrace();
+            }
+        }
+        else {
+            try{
+                AppendObjectOutputStream oos = new AppendObjectOutputStream(new FileOutputStream("container.txt", true));
+                oos.writeObject(container);
+                oos.close();
+            } catch (FileNotFoundException e){
+                e.printStackTrace();
+            } catch (IOException e){
+                e.printStackTrace();
+            }
+        }
+    }
     public static void createNewContainer(){
         Scanner scanner = new Scanner(System.in);
         int selection = 0;
+        System.out.println("Please enter container name");
+        String name = scanner.nextLine();
         while (true){
             System.out.println("Choose 0 for Dry Storage, 1 for Open Top, 2 for Open Side, 3 for Refridgerated, 4 for Liquid");
             selection = scanner.nextInt();
@@ -31,9 +58,6 @@ public class Container {
                 break;
             }
         }
-
-        System.out.println("Please enter container name");
-        String name = scanner.nextLine();
 
         System.out.println("Please enter container weight");
         double weight = scanner.nextDouble();
@@ -43,26 +67,37 @@ public class Container {
         for (int i =0; i<=10; i++){
             Cid = Cid + random_id.nextInt(10);
         }
+        File file = new File("container.txt");
 
         if (selection == 0){
-            Dry_Storage new_container = new Dry_Storage("DS" + Cid, name, weight);
-            container_list.add(new_container);
+            Container new_container = new Container("DS" + Cid, name, weight);
+            new_container.fuel_consumption_per_km_on_ship = 3.5;
+            new_container.fuel_consumption_per_km_on_truck = 4.6;
+            inputContainerIntoFile(file, new_container);
         }
         if (selection == 1){
-            Open_Top new_container = new Open_Top("OT" + Cid, name, weight);
-            container_list.add(new_container);
+            Container new_container = new Container("OT" + Cid, name, weight);
+            new_container.fuel_consumption_per_km_on_ship = 2.8;
+            new_container.fuel_consumption_per_km_on_truck = 3.2;
+            inputContainerIntoFile(file, new_container);
         }
         if (selection == 2){
-            Open_Side new_container = new Open_Side("OS" + Cid, name, weight);
-            container_list.add(new_container);
+            Container new_container = new Container("DS" + Cid, name, weight);
+            new_container.fuel_consumption_per_km_on_ship = 2.7;
+            new_container.fuel_consumption_per_km_on_truck = 3.2;
+            inputContainerIntoFile(file, new_container);
         }
         if (selection == 3){
-            Refridgerated new_container = new Refridgerated("RE" + Cid, name, weight);
-            container_list.add(new_container);
+            Container new_container = new Container("DS" + Cid, name, weight);
+            new_container.fuel_consumption_per_km_on_ship = 4.5;
+            new_container.fuel_consumption_per_km_on_truck = 5.4;
+            inputContainerIntoFile(file, new_container);
         }
-        else{
-            Liquid new_container = new Liquid("LI" + Cid, name, weight);
-            container_list.add(new_container);
+        if (selection == 4){
+            Container new_container = new Container("DS" + Cid, name, weight);
+            new_container.fuel_consumption_per_km_on_ship = 4.8;
+            new_container.fuel_consumption_per_km_on_truck = 5.3;
+            inputContainerIntoFile(file, new_container);
         }
 
     }
@@ -83,8 +118,31 @@ public class Container {
         return this.getFuel_consumption_per_km_on_truck();
     }
 
+    public static ArrayList<Container> getContainer(){
+        System.out.println("reading");
+        ArrayList<Container> container_list = new ArrayList<>();
+        try {
+            ObjectInputStream ois = new ObjectInputStream(new FileInputStream("container.txt"));
+            while (true){
+                try {
+                    Container container = (Container) ois.readObject();
+                    container_list.add(container);
+                } catch (EOFException e){
+                    break;
+                } catch (ClassNotFoundException e){
+                    e.printStackTrace();
+                }
+            }
+        } catch (FileNotFoundException e){
+            e.printStackTrace();
+        } catch (IOException e){
+            e.printStackTrace();
+        }
+        return container_list;
+    }
+
     public static Container queryContainerbyID(String Cid){
-        for (Container container: container_list){
+        for (Container container: getContainer() ){
             if (container.Cid.equals(Cid)){
                 return container;
             }
@@ -108,38 +166,3 @@ class Dry_Storage extends Container{
     }
 }
 
-class Open_Top extends Container{
-      double fuel_consumption_per_km_on_ship = 2.8;
-      double fuel_consumption_per_km_on_truck = 3.2;
-
-      public Open_Top(String Cid, String name, double weight){
-        super(Cid, name, weight);
-    }
-}
-
-class Open_Side extends Container{
-    double fuel_consumption_per_km_on_ship = 2.7;
-    double fuel_consumption_per_km_on_truck = 3.2;
-
-    public Open_Side(String Cid, String name, double weight){
-        super(Cid, name, weight);
-    }
-}
-
-class Refridgerated extends Container{
-    double fuel_consumption_per_km_on_ship = 4.5;
-    double fuel_consumption_per_km_on_truck = 5.4;
-
-    public Refridgerated(String Cid, String name, double weight){
-        super(Cid, name, weight);
-    }
-}
-
-class Liquid extends Container{
-    double fuel_consumption_per_km_on_ship = 4.8;
-    double fuel_consumption_per_km_on_truck = 5.3;
-
-    public Liquid(String Cid, String name, double weight){
-        super(Cid, name, weight);
-    }
-}
