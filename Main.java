d726e1d93f31651f045c86423bb26e90f8b0085d
trungluong0806 <141601693+trungluong080699@users.noreply.github.com>
2023-09-13 16:26:16 +0700

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,32 +1,12 @@
+import Users.Port_Manager;
+import Users.System_Admin;
+
 import java.io.*;
-import java.util.ArrayList;
-import java.util.Date;
-import java.util.Scanner;
 
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        System_Admin testing_admin = new System_Admin("1234567890", "Testing_Admin", "1234567890", "admin");
-        //System_Admin.addPort();
-        //System_Admin.addPort();
-        //System_Admin.addVehicle();
-        //System_Admin.addVehicle();
-        //System_Admin.addContainer();
-        //System_Admin.addManager();
-        Port_Manager manager = FileIOUtil.ReadManagerFromFile().get(0);
-        //manager.commenceTrip();
-        //manager.loadingContainersonVehicle();
-        //manager.completeTrip();
-        //manager.loadingContaineroffVehicle();
-        manager.RegisteringTrip();
-        //System.out.println(FileIOUtil.ReadTripFromFile().get(0));
-        //System_Admin.addPort();
-        //System_Admin.addPort();
-        //System.out.println(Port.queryPortbyID("0599540794").distanceCalc(Port.queryPortbyID("7186045282")));
-
-
-
-        //2679861123
+        Adminstration.program_Run();
 
 
 
